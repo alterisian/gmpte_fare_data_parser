a76518a8005172551665aa5236b0c41962035e92
Ian Moss <ian@alteris.co.uk>
2010-10-30 15:40:23 +0100

    Add Usage and TODO

--- lib/read_operators.rb
+++ lib/read_operators.rb
@@ -1,16 +1,23 @@
 # Read The Operators and populate the Operator Model
-filename = '../data/gmpte_foi/FARETABLES/Operators/Operators.txt'
+# Usage: from / of repo,
+# ruby lib/read_operators
+
+filename = 'data/gmpte_foi/FARETABLES/Operators/Operators.txt'
 line_num=0
 file = File.open(filename, "r") 
 file.each_line do |line|
   match=line.scan /([A-Z][A-Z][A-Z])     ([\w ]* )/
+  
+  # Ignore the header, and any non-matches
   if line_num > 0 and !match.first.nil?
     puts "code:"+match.first.first unless match.first.first.nil?
-    puts "name:"+match.first.last unless match.first.last.nil?
+    puts "name:"+match.first.last.strip unless match.first.last.nil?
   end
   line_num+=1
 end
 
-puts "read #{line_num} lines"
+puts "read #{line_num-1} operators"
+
+#TODO-Populate Rails App Operator Model
 
 
